70defcac7a08cd6d734d6eb5245392bb6148ba4f
meiji.yasuda <hem25jpn@yahoo.ne.jp>
2019-10-07 13:11:56 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
   root to: 'tasks#index'
+  resources :tasks
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
